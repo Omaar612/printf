71a3d7050b7a0dc4a9a15daf2a741825f35154e4
Omaar612 <gameromar483@gmail.com>
2023-04-23 22:25:17 -0700

    update

--- main.h
+++ main.h
@@ -35,7 +35,7 @@ struct fmt
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+va_list ap, char buff[], int flags, int width, int precision, int size);
 
 /* FUNCTIONS */
 
