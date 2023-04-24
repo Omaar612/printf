44c92098132afa4262a1aa3da72b03881d886c42
Omaar612 <gameromar483@gmail.com>
2023-04-23 21:58:48 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+void write_buff(char buff[], int *index);
 
 /**
  * _printf - Printf function
@@ -9,19 +9,51 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-int i, j = 0;
-int index = 0;
-char buff[buff_size];
+int i, j = 0, k = 0, s = 0, index = 0;
+int flags, width, precision, size;
+char buff[index];
 va_list ap;
 if (format == NULL)
 	return (-1);
 va_start(ap, format);
-for (i = 0; buff[buff_size] != '\0' && format; i++)
+for (i = 0; buff[s] != '\0' && format; i++)
 {
 if (format[i] != '%')
 {
+buff[s++] = format[i];
+if (index == s)
+{
+	write_buff(buff, &s);
+	j++;
+}
+}
+else
+{
+write_buff(buff, &s);
+flags = get_flags(format, &i);
+width = get_width(format, &i, ap);
+precision = get_precision(format, &i, ap);
+size = get_size(format, &i);
+++i;
+k = handle_print(format, &i, ap, buff,
+flags, width, precision, size);
+if (k == -1)
+return (-1);
+j += k;
 }
 }
 va_end(ap);
 return (j);
+}
+
+/**
+ * write_buff - Prints the contents of the buffer.
+ * @buff: Array of chars.
+ * @index: Index of the next char to be added.
+ */
+void write_buff(char buff[], int *index)
+{
+if (*index > 0)
+write(1, &buff[0], *index);
+*index = 0;
 }
